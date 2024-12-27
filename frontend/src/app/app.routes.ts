c54266d6192485b16ef71653d878449df2f1b7cd
Adrien2MN <adrien.de_mailly_nesle@edu.devinci.fr>
2024-12-27 18:45:45 +0100

    Link between app and grade-config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GradeConfigComponent } from './grade-config/grade-config.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'grade-config', component: GradeConfigComponent },
+];
---
+++ frontend/src/app/grade-config/grade-config.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-config',
+  standalone: true,
+  imports: [],
+  templateUrl: './grade-config.component.html',
+  styleUrl: './grade-config.component.css'
+})
+export class GradeConfigComponent {
+
+}
