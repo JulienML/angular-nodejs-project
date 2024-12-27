2523710bbd0be93efc1eff305a666655613eb437
JulienML <julien-devos@live.fr>
2024-12-27 20:15:11 +0100

    create backend with swagger and connect database

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Julien DE VOS, Adrien DE MAILLY NESLE, Thomas GOUTORBE, Mathieu COWAN
 `ng serve` from the frontend folder<br>
 `npm start` from the backend folder<br>
 Connect on `http://localhost:4200/`
-
-julien's branch
--- backend/app.ts
+++ backend/app.ts
@@ -1,16 +1,95 @@
 import express, { Request, Response } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import * as swaggerUi from 'swagger-ui-express';
+import { Sequelize, DataTypes } from 'sequelize';
 import cors from 'cors';
 
+// PostgreSQL database setup
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'admin',
+  database: 'SchoolDb',
+})
+
+// Model definition
+const Student = sequelize.define('Student', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+const Subject = sequelize.define('Subject', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+})
+
+const Mark = sequelize.define('Mark', {
+  id_student: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  id_subject: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  mark: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  coefficient: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1
+  }
+})
+
+// Synchronizing the database
+sequelize.sync();
+
+const initializeDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the PostgreSQL database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the PostgreSQL database:', error);
+  }
+};
+
+initializeDatabase();
+
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Node.js with TypeScript!');
-});
+const jsDocOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Node.js with TypeScript API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['app.ts'],
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(jsDocOptions)));
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
 });
