7dd2638d20a20ca97b6896db398d0085eba87531
Adrien2MN <adrien.de_mailly_nesle@edu.devinci.fr>
2024-12-27 14:24:23 +0100

    Création des fichiers de configuration-note component

---
+++ frontend/src/app/configuration-note/configuration-note.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-note',
+  standalone: true,
+  imports: [],
+  templateUrl: './configuration-note.component.html',
+  styleUrl: './configuration-note.component.css'
+})
+export class ConfigurationNoteComponent {
+
+}
