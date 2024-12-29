93c818477b6aafea60a3cbeed71a9e43543b7034
JulienML <julien-devos@live.fr>
2024-12-29 11:12:09 +0100

    add all-marks page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { GradeConfigComponent } from './grade-config/grade-config.component';
 import { GradeVisualizationComponent } from './grade-visualization/grade-visualization.component';
 import { MainMarksComponent } from './mainmarks/mainmarks.component';
+import { AllMarksComponent } from './allmarks/allmarks.component';
 
 export const routes: Routes = [
   { path: 'grade-config', component: GradeConfigComponent },
   { path: 'charts', redirectTo: 'grade-visualization', pathMatch: 'full' },
   { path: 'grade-visualization', component: GradeVisualizationComponent },
-  { path: '', redirectTo: '/grade-config', pathMatch: 'full' },
+  { path: '', redirectTo: '/mainmarks', pathMatch: 'full' },
   { path: 'mainmarks', component: MainMarksComponent },
+  { path: 'all-marks', component: AllMarksComponent }
 ];
